503529055331fdafdd2dc1807498784e09cd3641
Kalidhas V Biju <kalidhas440@gmail.com>
2025-03-17 20:16:07 +0530

    --no-message

--- scripts/restore_brave_sessions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-LAST_SHUTDOWN=$(journalctl --list-boots | tail -2 | head -1 | awk '{print $1}')
-CRASHED=$(journalctl -b "$LAST_SHUTDOWN" -n 50 | grep -i "unexpected shutdown")
-
-if [[ -n "$CRASHED" ]]; then
-    brave --restore-last-session &
-fi
-
