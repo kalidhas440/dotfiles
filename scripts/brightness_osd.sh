45a04a3eb5a659123f4c213a06602b37ed4820fc
Kalidhas V Biju <kalidhas440@gmail.com>
2025-03-20 00:34:10 +0530

    --no-message

--- scripts/brightness_osd.sh
+++ scripts/brightness_osd.sh
@@ -3,5 +3,5 @@ MAX=$(brightnessctl max)
 CURRENT=$(brightnessctl get)
 PERCENT=$((CURRENT * 100 / MAX))
 
-notify-send "󰃠 Brightness: $PERCENT%" --expire-time=1000
-
+notify-send -u low "󰃠 Brightness: $PERCENT%" --expire-time=1500
+sleep 1.5 && swaync-client --close-latest 
--- scripts/volume_osd.sh
+++ scripts/volume_osd.sh
@@ -4,6 +4,7 @@ VOLUME=$(pamixer --get-volume)
 if pamixer --get-mute | grep -q true; then
     notify-send "󰝟 Muted" --expire-time=1000
 else
-    notify-send " Volume: $VOLUME%" --expire-time=1000
+    notify-send -u low " Volume: $VOLUME%" --expire-time=1500
+    sleep 1.5 && swaync-client --close-latest
 fi
 
