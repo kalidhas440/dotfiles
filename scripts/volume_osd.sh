9e3bf93e5c180692dee3a0c5e2c544d44a3287b2
Kalidhas V Biju <kalidhas440@gmail.com>
2026-05-01 16:07:12 +0530

    --no-message

--- scripts/volume_osd.sh
+++ scripts/volume_osd.sh
@@ -3,9 +3,9 @@ VOLUME=$(pamixer --get-volume)
 
 if pamixer --get-mute | grep -q true; then
     notify-send -a osd -h string:x-canonical-private-synchronous:osd "󰝟 Muted" #--expire-time=1500
-    sleep 1.5 && swaync-client --close-latest
+    #sleep 1.5 && swaync-client --close-latest
 else
     notify-send -a osd -h string:x-canonical-private-synchronous:osd " Volume: $VOLUME%" #--expire-time=1500
-    sleep 3 && swaync-client --close-latest
+    #sleep 3 && swaync-client --close-latest
 fi
 
